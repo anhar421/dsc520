399a6a95c092d128f89b515db2343de8fdcccce3
Anna Harvey <anharvey@my365.bellevue.edu>
2020-06-07 20:35:00 -0400

    Update 1.1_TestScores_HarveyAnna.R

--- assignments/1.1_TestScores_HarveyAnna/1.1_TestScores_HarveyAnna.R
+++ assignments/1.1_TestScores_HarveyAnna/1.1_TestScores_HarveyAnna.R
@@ -38,7 +38,30 @@ plot(Score~Count, data = sports_sec, col = "red", main = "Sports Section Scores"
 #         section (sports had 19 different scores and regular had 17 different
 #         scores). It also had a wider range of scores (sports range = 200 - 395;
 #         regular range = 265 - 380).
-#         Overall, the average score for students in the regular section is higher.
-#         This can be seen in the fact that 210 students scored 300 or higher in
-#         the regular section, while only 170 students scored 320 or higher in
-#         the sports section.
+#         However, the average score for students in the regular section is higher.
+#         This can be seen in the fact that 260 students scored 300 or higher in
+#         the regular section, while only 220 students scored 320 or higher in
+#         the sports section. (Scoring 300-400 points would be the upper quartile
+#         of the data if we assume the range is 0-400 possible points). (Note that
+#         there seemed to be duplicate rows in the data for the regular section
+#         which could cause errors in analysis.)
+
+# b. Did every student in one section score more points than every student in the 
+#    other section? If not, explain what a statistical tendency means in this context.
+# Answer: Neither section had every student score more points then every student
+#         in the other section. In this context, the statistical tendency would most 
+#         likely be the mode (the most frequent score in each section). For the 
+#         sports section, the mode was 285 and 335 (both had 30 students with
+#         those scores). The regular section mode was 350 (30 students).
+
+# c. What could be one additional variable that was not mentioned in the narrative 
+#    that could be influencing the point distributions between the two sections?
+# Answer: Another variable that could be influencing the point distribution may be
+#         a demographic difference between the two sections. It is stated that the
+#         sports-themed section was advertised as such to the students prior to 
+#         registration. It is possible that the students who were more likely to 
+#         pick a sports-themed class would score differently on average than their
+#         their counterparts, particularly when only given sports examples. It is
+#         also possible that the students who specifically chose NOT to register
+#         for a sports-themed class would be influenced differently by a more
+#         diverse teaching method.
