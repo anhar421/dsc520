7a5b257bdd945a1f240aaab9f08e38423e549a5d
Anna Harvey <anharvey@my365.bellevue.edu>
2020-07-30 18:47:35 -0400

    Update finalproject.R

--- finalproject.R
+++ finalproject.R
@@ -32,15 +32,14 @@ fl_2014_econ <- select(fl_2014, DP03_0051E, DP03_0051M,
 fl_econ_5yr <- rbind(fl_2014_econ, fl_2015_econ, fl_2016_econ, 
                           fl_2017_econ, fl_2018_econ)
 
+#Remove unneccesary metadata
+fl_econ_5yr <- fl_econ_5yr[-c(1, 3, 5, 7, 9), ]
+
 #Rename columns
 colnames(fl_econ_5yr) <- c("Total_households","MarginofError_TH", "Median_household_income",
                            "MarginofError_MedHI", "Mean_household_income", "MarginofError_MeanHI")
+rownames(fl_econ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
-#Remove unneccesary metadata
-fl_econ_5yr <- fl_econ_5yr[-c(1, 3, 5, 7, 9), ]
-fl_econ_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
-fl_econ_5yr <- fl_econ_5yr[,c("year", "Total_households","MarginofError_TH", "Median_household_income",
-                              "MarginofError_MedHI", "Mean_household_income", "MarginofError_MeanHI")]
 
 #2018 New York data
 ny_2018 <- read.csv("NY_ACSDP1Y2018.DP03_data_with_overlays_2020-07-29T142230.csv")
@@ -70,15 +69,50 @@ ny_2014_econ <- select(ny_2014, DP03_0051E, DP03_0051M,
 ny_econ_5yr <- rbind(ny_2014_econ, ny_2015_econ, ny_2016_econ, 
                      ny_2017_econ, ny_2018_econ)
 
+#Remove unneccesary metadata
+ny_econ_5yr <- ny_econ_5yr[-c(1, 3, 5, 7, 9), ]
+
 #Rename columns
 colnames(ny_econ_5yr) <- c("Total_households","MarginofError_TH", "Median_household_income",
                            "MarginofError_MedHI", "Mean_household_income", "MarginofError_MeanHI")
+rownames(ny_econ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
+
+
+#Import 2018 Washington data as dataframe
+wa_2018 <- read.csv("ACSDP1Y2018.DP03_data_with_overlays_2020-07-30T170401.csv")
+wa_2018_econ <- select(wa_2018, DP03_0051E, DP03_0051M,
+                       DP03_0062E, DP03_0062M, DP03_0063E, DP03_0063M)
+
+#2017 Washington data
+wa_2017 <- read.csv("ACSDP1Y2017.DP03_data_with_overlays_2020-07-30T170401.csv")
+wa_2017_econ <- select(wa_2017, DP03_0051E, DP03_0051M,
+                       DP03_0062E, DP03_0062M, DP03_0063E, DP03_0063M)
+
+#2016 Washington data
+wa_2016 <- read.csv("ACSDP1Y2016.DP03_data_with_overlays_2020-07-30T170401.csv")
+wa_2016_econ <- select(wa_2016, DP03_0051E, DP03_0051M,
+                       DP03_0062E, DP03_0062M, DP03_0063E, DP03_0063M)
+
+#2015 Washington data
+wa_2015 <- read.csv("ACSDP1Y2015.DP03_data_with_overlays_2020-07-30T170401.csv")
+wa_2015_econ <- select(wa_2015, DP03_0051E, DP03_0051M,
+                       DP03_0062E, DP03_0062M, DP03_0063E, DP03_0063M)
+
+#2015 Washington data
+wa_2014 <- read.csv("ACSDP1Y2014.DP03_data_with_overlays_2020-07-30T170401.csv")
+wa_2014_econ <- select(wa_2014, DP03_0051E, DP03_0051M,
+                       DP03_0062E, DP03_0062M, DP03_0063E, DP03_0063M)
+
+wa_econ_5yr <- rbind(wa_2014_econ, wa_2015_econ, wa_2016_econ, 
+                     wa_2017_econ, wa_2018_econ)
 
 #Remove unneccesary metadata
-ny_econ_5yr <- ny_econ_5yr[-c(1, 3, 5, 7, 9), ]
-ny_econ_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
-ny_econ_5yr <- ny_econ_5yr[,c("year", "Total_households","MarginofError_TH", "Median_household_income",
-                              "MarginofError_MedHI", "Mean_household_income", "MarginofError_MeanHI")]
+wa_econ_5yr <- wa_econ_5yr[-c(1, 3, 5, 7, 9), ]
+
+#Rename columns
+colnames(wa_econ_5yr) <- c("Total_households","MarginofError_TH", "Median_household_income",
+                           "MarginofError_MedHI", "Mean_household_income", "MarginofError_MeanHI")
+rownames(wa_econ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
 #HOUSEHOLD DATA
 #Florida
@@ -100,10 +134,11 @@ flhh_2014_smol <- select(flhh_2014, S1101_C01_001E, S1101_C01_001M, S1101_C01_00
 fl_hh_5yr <- rbind(flhh_2014_smol, flhh_2015_smol, flhh_2016_smol, 
                    flhh_2017_smol, flhh_2018_smol)
 
+fl_hh_5yr <- fl_hh_5yr[-c(1, 3, 5, 7, 9), ]
+
 colnames(fl_hh_5yr) <- c("Total_households","MarginofError_TH", "Avg_household_size",
                            "MarginofError_AvgHHsz")
-
-fl_hh_5yr <- fl_hh_5yr[-c(1, 3, 5, 7, 9), ]
+rownames(fl_hh_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
 
 #New York
@@ -125,13 +160,37 @@ nyhh_2014_smol <- select(nyhh_2014, S1101_C01_001E, S1101_C01_001M, S1101_C01_00
 ny_hh_5yr <- rbind(nyhh_2014_smol, nyhh_2015_smol, nyhh_2016_smol, 
                    nyhh_2017_smol, nyhh_2018_smol)
 
+ny_hh_5yr <- ny_hh_5yr[-c(1, 3, 5, 7, 9), ]
+
 colnames(ny_hh_5yr) <- c("Total_households","MarginofError_TH", "Avg_household_size",
                          "MarginofError_AvgHHsz")
+rownames(ny_hh_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
-ny_hh_5yr <- ny_hh_5yr[-c(1, 3, 5, 7, 9), ]
-ny_hh_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
-ny_hh_5yr <- ny_hh_5yr[,c("year", "Total_households","MarginofError_TH", "Avg_household_size",
-                          "MarginofError_AvgHHsz")]
+#Washington
+wahh_2018 <- read.csv("Household data/ACSST1Y2018.S1101_data_with_overlays_2020-07-30T170119.csv")
+wahh_2018_smol <- select(wahh_2018, S1101_C01_001E, S1101_C01_001M, S1101_C01_002E, S1101_C01_002M)
+
+wahh_2017 <- read.csv("Household data/ACSST1Y2017.S1101_data_with_overlays_2020-07-30T170119.csv")
+wahh_2017_smol <- select(wahh_2017, S1101_C01_001E, S1101_C01_001M, S1101_C01_002E, S1101_C01_002M)
+
+wahh_2016 <- read.csv("Household data/ACSST1Y2016.S1101_data_with_overlays_2020-07-30T170119.csv")
+wahh_2016_smol <- select(wahh_2016, S1101_C01_001E, S1101_C01_001M, S1101_C01_002E, S1101_C01_002M)
+
+wahh_2015 <- read.csv("Household data/ACSST1Y2015.S1101_data_with_overlays_2020-07-30T170119.csv")
+wahh_2015_smol <- select(wahh_2015, S1101_C01_001E, S1101_C01_001M, S1101_C01_002E, S1101_C01_002M)
+
+wahh_2014 <- read.csv("Household data/ACSST1Y2014.S1101_data_with_overlays_2020-07-30T170119.csv")
+wahh_2014_smol <- select(wahh_2014, S1101_C01_001E, S1101_C01_001M, S1101_C01_002E, S1101_C01_002M)
+
+wa_hh_5yr <- rbind(wahh_2014_smol, wahh_2015_smol, wahh_2016_smol, 
+                   wahh_2017_smol, wahh_2018_smol)
+
+wa_hh_5yr <- wa_hh_5yr[-c(1, 3, 5, 7, 9), ]
+
+colnames(wa_hh_5yr) <- c("Total_households","MarginofError_TH", "Avg_household_size",
+                         "MarginofError_AvgHHsz")
+rownames(wa_hh_5yr) <- c("2014", "2015", "2016", "2017", "2018")
+
 
 #OCCUPATION INFORMATION
 #Florida
@@ -177,16 +236,7 @@ fl_occ_2014 <- select(fl_2014, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, D
 
 fl_occ_5yr <- rbind(fl_occ_2014, fl_occ_2015, fl_occ_2016, fl_occ_2017, fl_occ_2018)
 fl_occ_5yr <- fl_occ_5yr[-c(1, 3, 5, 7, 9), ]
-fl_occ_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
-fl_occ_5yr <- fl_occ_5yr[,c("year", "DP03_0026E", "DP03_0026M", "DP03_0027E", "DP03_0027M", 
-                            "DP03_0028E", "DP03_0028M", "DP03_0029E", "DP03_0029M", "DP03_0030E", 
-                            "DP03_0030M", "DP03_0031E", "DP03_0031M", "DP03_0032E", "DP03_0032M", 
-                            "DP03_0033E", "DP03_0033M", "DP03_0034E", "DP03_0034M", "DP03_0035E", 
-                            "DP03_0035M", "DP03_0036E", "DP03_0036M", "DP03_0037E", "DP03_0037M",
-                            "DP03_0038E", "DP03_0038M", "DP03_0039E", "DP03_0039M", "DP03_0040E", 
-                            "DP03_0040M", "DP03_0041E", "DP03_0041M", "DP03_0042E", "DP03_0042M", 
-                            "DP03_0043E", "DP03_0043M", "DP03_0044E", "DP03_0044M", "DP03_0045E", 
-                            "DP03_0045M")]
+rownames(fl_occ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
 #New York
 ny_occ_2018 <- select(ny_2018, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
@@ -231,26 +281,79 @@ ny_occ_2014 <- select(ny_2014, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, D
 
 ny_occ_5yr <- rbind(ny_occ_2014, ny_occ_2015, ny_occ_2016, ny_occ_2017, ny_occ_2018)
 ny_occ_5yr <- ny_occ_5yr[-c(1, 3, 5, 7, 9), ]
-ny_occ_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
-ny_occ_5yr <- ny_occ_5yr[,c("year", "DP03_0026E", "DP03_0026M", "DP03_0027E", "DP03_0027M", 
-                            "DP03_0028E", "DP03_0028M", "DP03_0029E", "DP03_0029M", "DP03_0030E", 
-                            "DP03_0030M", "DP03_0031E", "DP03_0031M", "DP03_0032E", "DP03_0032M", 
-                            "DP03_0033E", "DP03_0033M", "DP03_0034E", "DP03_0034M", "DP03_0035E", 
-                            "DP03_0035M", "DP03_0036E", "DP03_0036M", "DP03_0037E", "DP03_0037M",
-                            "DP03_0038E", "DP03_0038M", "DP03_0039E", "DP03_0039M", "DP03_0040E", 
-                            "DP03_0040M", "DP03_0041E", "DP03_0041M", "DP03_0042E", "DP03_0042M", 
-                            "DP03_0043E", "DP03_0043M", "DP03_0044E", "DP03_0044M", "DP03_0045E", 
-                            "DP03_0045M")]
+rownames(ny_occ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
-median_income_5yr <- cbind(ny_econ_5yr$year, ny_econ_5yr$Median_household_income, 
-                           fl_econ_5yr$Median_household_income)
+#Washington
+wa_occ_2018 <- select(wa_2018, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
+                      DP03_0029E, DP03_0029M, DP03_0030E, DP03_0030M, DP03_0031E, DP03_0031M, 
+                      DP03_0032E, DP03_0032M, DP03_0033E, DP03_0033M, DP03_0034E, DP03_0034M,
+                      DP03_0035E, DP03_0035M, DP03_0036E, DP03_0036M, DP03_0037E, DP03_0037M,
+                      DP03_0038E, DP03_0038M, DP03_0039E, DP03_0039M, DP03_0040E, DP03_0040M,
+                      DP03_0041E, DP03_0041M, DP03_0042E, DP03_0042M, DP03_0043E, DP03_0043M,
+                      DP03_0044E, DP03_0044M, DP03_0045E, DP03_0045M)
+
+wa_occ_2017 <- select(wa_2017, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
+                      DP03_0029E, DP03_0029M, DP03_0030E, DP03_0030M, DP03_0031E, DP03_0031M, 
+                      DP03_0032E, DP03_0032M, DP03_0033E, DP03_0033M, DP03_0034E, DP03_0034M,
+                      DP03_0035E, DP03_0035M, DP03_0036E, DP03_0036M, DP03_0037E, DP03_0037M,
+                      DP03_0038E, DP03_0038M, DP03_0039E, DP03_0039M, DP03_0040E, DP03_0040M,
+                      DP03_0041E, DP03_0041M, DP03_0042E, DP03_0042M, DP03_0043E, DP03_0043M,
+                      DP03_0044E, DP03_0044M, DP03_0045E, DP03_0045M)
+
+wa_occ_2016 <- select(wa_2016, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
+                      DP03_0029E, DP03_0029M, DP03_0030E, DP03_0030M, DP03_0031E, DP03_0031M, 
+                      DP03_0032E, DP03_0032M, DP03_0033E, DP03_0033M, DP03_0034E, DP03_0034M,
+                      DP03_0035E, DP03_0035M, DP03_0036E, DP03_0036M, DP03_0037E, DP03_0037M,
+                      DP03_0038E, DP03_0038M, DP03_0039E, DP03_0039M, DP03_0040E, DP03_0040M,
+                      DP03_0041E, DP03_0041M, DP03_0042E, DP03_0042M, DP03_0043E, DP03_0043M,
+                      DP03_0044E, DP03_0044M, DP03_0045E, DP03_0045M)
+
+wa_occ_2015 <- select(wa_2015, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
+                      DP03_0029E, DP03_0029M, DP03_0030E, DP03_0030M, DP03_0031E, DP03_0031M, 
+                      DP03_0032E, DP03_0032M, DP03_0033E, DP03_0033M, DP03_0034E, DP03_0034M,
+                      DP03_0035E, DP03_0035M, DP03_0036E, DP03_0036M, DP03_0037E, DP03_0037M,
+                      DP03_0038E, DP03_0038M, DP03_0039E, DP03_0039M, DP03_0040E, DP03_0040M,
+                      DP03_0041E, DP03_0041M, DP03_0042E, DP03_0042M, DP03_0043E, DP03_0043M,
+                      DP03_0044E, DP03_0044M, DP03_0045E, DP03_0045M)
+
+wa_occ_2014 <- select(wa_2014, DP03_0026E, DP03_0026M, DP03_0027E, DP03_0027M, DP03_0028E, DP03_0028M,
+                      DP03_0029E, DP03_0029M, DP03_0030E, DP03_0030M, DP03_0031E, DP03_0031M, 
+                      DP03_0032E, DP03_0032M, DP03_0033E, DP03_0033M, DP03_0034E, DP03_0034M,
+                      DP03_0035E, DP03_0035M, DP03_0036E, DP03_0036M, DP03_0037E, DP03_0037M,
+                      DP03_0038E, DP03_0038M, DP03_0039E, DP03_0039M, DP03_0040E, DP03_0040M,
+                      DP03_0041E, DP03_0041M, DP03_0042E, DP03_0042M, DP03_0043E, DP03_0043M,
+                      DP03_0044E, DP03_0044M, DP03_0045E, DP03_0045M)
+
+wa_occ_5yr <- rbind(wa_occ_2014, wa_occ_2015, wa_occ_2016, wa_occ_2017, wa_occ_2018)
+wa_occ_5yr <- wa_occ_5yr[-c(1, 3, 5, 7, 9), ]
+rownames(wa_occ_5yr) <- c("2014", "2015", "2016", "2017", "2018")
+
+#Put all relevant data into single dataframe for household and income variables
+summary_5yr <- as.data.frame(cbind(ny_econ_5yr$Median_household_income, 
+                           fl_econ_5yr$Median_household_income, wa_econ_5yr$Median_household_income,
+                           ny_econ_5yr$Mean_household_income,fl_econ_5yr$Mean_household_income,
+                           wa_econ_5yr$Mean_household_income, ny_econ_5yr$Total_households,
+                           fl_econ_5yr$Total_households, wa_econ_5yr$Total_households,
+                           ny_hh_5yr$Avg_household_size,fl_hh_5yr$Avg_household_size, 
+                           wa_hh_5yr$Avg_household_size))
+
+summary_5yr$year <- c("2014", "2015", "2016", "2017", "2018")
+colnames(summary_5yr) <- c("NY_Med_Inc", "FL_Med_Inc", "WA_Med_Inc", "NY_Mn_Inc", 
+                           "FL_Mn_Inc", "WA_Mn_Inc", "NY_Total_HH", "FL_Total_HH",
+                           "WA_Total_HH", "NY_HH_size","FL_HH_size", "WA_HH_size", "Year")
+rownames(summary_5yr) <- c("2014", "2015", "2016", "2017", "2018")
 
 
-ggplot() +
-    geom_line(data = median_income_5yr, aes(x = year, y = Median_household_income, group = 1),
-              color = "blue") +
-    geom_line(data = fl_econ_5yr, aes(x = year, y = Median_household_income, group = 1),
-              color = "red") +
-    xlab('Year') +
-    ylab('Median Household Income (dollars)')
+
+ggplot(summary_5yr) + 
+    geom_line(aes(x = Year, y = NY_Med_Inc, group = 1, color ="blue")) +
+    geom_line(aes(x = Year, y = FL_Med_Inc, group = 1, color = "red")) +
+    geom_line(aes(x = Year, y = WA_Med_Inc, group = 1, color = "green"))
+
+
+summary_occ_2014 <- as.data.frame((rbind(ny_occ_2014, fl_occ_2014, wa_occ_2014)))
+summary_occ_2014 <- summary_occ_2014[-c(1, 3, 5), ]
+summary_occ_2014$state <- c("NY", "FL", "WA")
+rownames(summary_occ_2014) <- c("NY", "FL", "WA")
+
 
